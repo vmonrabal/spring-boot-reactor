375cc5202065af8ef644d4231b6e0ecd6df70bb4
Vicente Monrabal <vmonrabal@gmail.com>
2019-12-16 20:31:44 +0100

    commit for course session 8

--- src/main/java/com/monrabal/springboot/reactor/app/SpringBootReactorApplication.java
+++ src/main/java/com/monrabal/springboot/reactor/app/SpringBootReactorApplication.java
@@ -1,5 +1,7 @@
 package com.monrabal.springboot.reactor.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,15 +10,23 @@ import reactor.core.publisher.Flux;
 @SpringBootApplication
 public class SpringBootReactorApplication implements CommandLineRunner {
 
+	private static final Logger log = LoggerFactory.getLogger(SpringBootReactorApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootReactorApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		Flux<String> nombres = Flux.just("Vicente", "Andrés", "Diego", "Alberto")
-				.doOnNext(System.out::println);
+		Flux<String> nombres = Flux.just("Vicente", "Andrés", "", "Diego", "Alberto")
+				.doOnNext(e -> {
+					if(e.isEmpty()){
+						throw new RuntimeException("Nombres no pueden estar vacíos");
+					} {
+						System.out.println(e);
+					}
+				});
 
-		nombres.subscribe();
+		nombres.subscribe(e -> log.info(e),
+				error -> log.error(error.getMessage()));
 	}
 }
