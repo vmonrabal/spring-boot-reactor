5aaeaecdc1c28f347f3882c5880376190227cd38
Vicente Monrabal <vmonrabal@gmail.com>
2019-12-18 20:25:10 +0100

    Reactor - filter exercise

--- src/main/java/com/monrabal/springboot/reactor/app/SpringBootReactorApplication.java
+++ src/main/java/com/monrabal/springboot/reactor/app/SpringBootReactorApplication.java
@@ -1,5 +1,6 @@
 package com.monrabal.springboot.reactor.app;
 
+import com.monrabal.springboot.reactor.app.models.Usuario;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -17,15 +18,22 @@ public class SpringBootReactorApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		Flux<String> nombres = Flux.just("Vicente", "Andrés", "Maria", "Diego", "Alberto")
-				.doOnNext(e -> {
-					if(e.isEmpty()){
+		Flux<Usuario> nombres = Flux.just("Vicente Monrabal", "Andrés Fulano", "Maria Fulana", "Diego Sultano", "Alberto Liao", "Bruce Lee", "Bruce Willis")
+				.map(nombre -> new Usuario(nombre.split(" ")[0].toUpperCase(), nombre.split(" ")[1].toUpperCase()))
+				.filter(usuario -> usuario.getNombre().toLowerCase().equals("bruce"))
+				.doOnNext(usuario -> {
+					if(usuario == null){
 						throw new RuntimeException("Nombres no pueden estar vacíos");
 					}
-					System.out.println(e);
+					System.out.println(usuario.getNombre()+ " " + usuario.getApellido());
+				})
+				.map(usuario -> {
+					String nombre = usuario.getNombre().toLowerCase();
+					usuario.setNombre(nombre);
+					return usuario;
 				});
 
-		nombres.subscribe(e -> log.info(e),
+		nombres.subscribe(e -> log.info(e.toString()),
 				error -> log.error(error.getMessage()),
 				new Runnable() {
 					@Override
