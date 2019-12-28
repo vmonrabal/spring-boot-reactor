b8b50a698ef84dbe4ef067dbd88e656184bcfcc1
Vicente Monrabal <vmonrabal@gmail.com>
2019-12-28 19:33:01 +0100

    Usando countdown y retry

--- src/main/java/com/monrabal/springboot/reactor/app/SpringBootReactorApplication.java
+++ src/main/java/com/monrabal/springboot/reactor/app/SpringBootReactorApplication.java
@@ -14,6 +14,7 @@ import reactor.core.publisher.Mono;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
 
 @SpringBootApplication
 public class SpringBootReactorApplication implements CommandLineRunner {
@@ -25,7 +26,26 @@ public class SpringBootReactorApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		ejemploDelayElements();
+		ejemploIntervalInfinito();
+	}
+
+	public void ejemploIntervalInfinito() throws InterruptedException {
+
+		CountDownLatch latch = new CountDownLatch(1);
+
+		Flux.interval(Duration.ofSeconds(1))
+		.doOnTerminate(latch::countDown)
+		.flatMap(i -> {
+			if( i >= 5){
+				return Flux.error(new InterruptedException("Solo hasta 5"));
+			}
+			return Flux.just(i);
+		})
+		.map(i -> "Hola " + i)
+		.retry(2)
+		.subscribe(s -> log.info(s), e -> log.error(e.getMessage()));
+
+		latch.await();
 	}
 
 	public void ejemploDelayElements(){
